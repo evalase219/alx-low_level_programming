15084b2c51a30104adf0ea22563b09e1c4606dcc
Evalase219 <Chiomaevangeline85@gmail.com>
2023-04-20 07:09:57 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line puts function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
