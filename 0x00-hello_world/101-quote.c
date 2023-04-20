1fe8f07e24d72b39b2d6544e7e0368a92c1dbc73
Evalase219 <Chiomaevangeline85@gmail.com>
2023-04-20 11:37:33 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - A c program that prints a line to the standard error
- * Return: 1 (Success)
+ *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
