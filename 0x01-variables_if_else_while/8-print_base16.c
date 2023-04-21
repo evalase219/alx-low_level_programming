dad8e7a8b90be464e62ecbddbaab0a59b49bd243
Evalase219 <Chiomaevangeline85@gmail.com>
2023-04-21 14:47:09 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all the numbers of base 16
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char ch;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
