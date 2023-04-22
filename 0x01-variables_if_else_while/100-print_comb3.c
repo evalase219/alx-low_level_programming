4edf8431179485711886f54a15999488b275b5ba
Evalase219 <Chiomaevangeline85@gmail.com>
2023-04-21 17:19:36 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int y, x;
 
-	for (y = 38; y <= 45; y++)
+	for (y = 48; y <= 56; y++)
 	{
-		for (x = 39; x <= 46; x++)
+		for (x = 49; x <= 57; x++)
 		{
 			if (x > y)
 			{
 				putchar(y);
 				putchar(x);
-				if (y != 45 || x != 46)
+				if (y != 49 || x != 57)
 				{
 					putchar(',');
 					putchar(' ');
