65cdb6fe66e97b25f4b5b8d3066f0ad099676fee
Evalase219 <Chiomaevangeline85@gmail.com>
2023-04-21 18:56:17 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,17 +13,17 @@ int main(void)
 		for (y = x + 1; y < 9; y++)
 		{
 			for (z = y + 1; z < 10; z++)
-				{
-					putchar((x % 10) + '0');
-					putchar((y % 10) + '0');
-					putchar((z % 10) + '0');
+			{
+				putchar((x % 10) + '0');
+				putchar((y % 10) + '0');
+				putchar((z % 10) + '0');
 
-					if (x == 7 && y == 8 && z == 9)
-						continue;
+				if (x == 7 && y == 8 && z == 9)
+					continue;
 
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 
