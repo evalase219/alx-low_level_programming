0c154609169509d693908e11b3369e9505285c36
Evalase219 <Chiomaevangeline85@gmail.com>
2023-04-25 13:51:52 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,22 +21,22 @@ void print_times_table(int n)
 				} else if (z < 10 && y != 0)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
+					_putchar('');
+					_putchar('');
+					_putchar('');
+					_putchar('');
 					_putchar(z + '0');
 				} else if (z >= 10 && z < 100)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
+					_putchar('');
+					_putchar('');
 					_putchar((z / 10) + '0');
 					_putchar((z % 10) + '0');
 				} else if (z >= 100)
 				{
 					_putchar(',');
-					_putchar(' ');
+					_putchar('');
 					_putchar((z / 100) + '0');
 					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
