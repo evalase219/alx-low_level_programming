c7c04a8c3eb5225aace43dbad88f51ecd519dbab
Evalase219 <Chiomaevangeline85@gmail.com>
2023-04-25 12:34:12 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,6 +25,7 @@ void print_times_table(int n)
 					_putchar(' ');
 				if (z <= 9)
 					_putchar(' ');
+
 				if (x >= 100)
 				{
 					_putchar((z / 10) + '0');
@@ -36,6 +37,7 @@ void print_times_table(int n)
 				}
 				_putchar((z % 10) + '0');
 			}
+			_putchar('\n');
 		}
 	}
 }
