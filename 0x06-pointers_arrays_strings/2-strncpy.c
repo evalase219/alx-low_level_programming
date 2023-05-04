ccfd107b502432f2cb4f4a902ea89c94cb5555f0
Evalase219 <Chiomaevangeline85@gmail.com>
2023-05-04 06:28:55 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,16 +9,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int x = 0, y = 0;
+	int x;
 
-	while (src[x++])
-		y++;
-
-	for (x = 0; src[x] && x < n; x++)
+	for (x = 0; x < n && src[x] != '\0'; x++)
 		dest[x] = src[x];
-
-	for (x = y; x < n; x++)
-		dest[x] = '\n';
+	for ( ; x < n; x++)
+		dest[x] = '\0';
 
 	return (dest);
 }
