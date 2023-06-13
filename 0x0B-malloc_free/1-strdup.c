aa83993374ae198e685114946f6bec5e0147eb13
evalase219 <chiomaevangeline85@gmail.com>
2023-06-13 06:04:43 -0700

    Allocation of new space for a duplicated string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,8 +16,14 @@ char *_strdup(char *str)
 	unsigned int len, x;
 
 	if (str == NULL)
+	{
 		return (NULL);
-	for (len = 0; str[len] != '\0'; len++)
+	}
+	len = 0;
+	while ((str[len]) != '\0')
+	{
+		len++;
+	}
 
 	newstr = malloc(sizeof(char) * (len + 1));
 	if (newstr == NULL)
