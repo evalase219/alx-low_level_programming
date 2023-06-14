601859f7ae61e7ff44b0a6216b588089ae202abf
evalase219 <chiomaevangeline85@gmail.com>
2023-06-14 13:23:21 -0700

    Two dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,35 +1,40 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
-* alloc_grid - function that returns a pointer to a two dimensional array
-* @width: width of the array to input.
-* @height: height of the array to input.
-* Return: pointer to two dimensional array.
-*/
+ * alloc_grid - Function that returns a pointer to 2 dimensional array
+ * of integer
+ * @width: The width of the array
+ * @height: The height of the array
+ *
+ * Return: On success, return a pointer to 2 dimensional array,
+ * on failure return NULL.
+ */
 int **alloc_grid(int width, int height)
 {
 	int **shape;
-	int x, y;
+	int m, n;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	shape = malloc(sizeof(int *) * height);
+
+	shape = malloc(height * sizeof(int));
 	if (shape == NULL)
 		return (NULL);
-	for (x = 0; x < height; x++)
-		shape[x] = malloc(sizeof(int) * width);
-	if (shape[x] == NULL)
+
+	for (m = 0; m < height ; m++)
 	{
-		for (; x >= 0; x--)
-			free(shape[x]);
-		free(shape);
-		return (NULL);
-	}
-	for (x = 0; x < height; x++)
-	{
-		for (y = 0; y < width; y++)
-			shape[x][y] = 0;
+		shape[m] = malloc(width * sizeof(int));
+		if (shape[m] == NULL)
+		{
+			while (m >= 0)
+				free(shape[m--]);
+			free(shape);
+			return (NULL);
+		}
+
+		for (n = 0; n < width; n++)
+			shape[m][n] = 0;
 	}
 	return (shape);
 }
